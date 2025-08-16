3cc6416d2fd07685d31c6d937141e94f64fca6b8
arafat-zihad <arafat.zihad01@gmail.com>
2025-08-16 22:05:04 +0600

    primitive data type

---
+++ Basic_Java/Data_typs.java
@@ -0,0 +1,11 @@
+public class{
+    public static void main(String[] args) {
+        //Primitive Data Typee
+        int num = 5;
+        float fnum = 6.99f;
+        char cletter = 'D';
+        boolean mybool = true;
+
+        String mytext = "Hello";
+    }
+}
