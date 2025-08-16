a9c10cae5dc4a080bb9dee6a9a384a088b7c6b4b
arafat-zihad <arafat.zihad01@gmail.com>
2025-08-16 22:15:45 +0600

    data types done

---
+++ Basic_Java/Data_types.java
@@ -0,0 +1,28 @@
+class Data_types {
+    public static void main(String[] args) {
+        //Primitive Data Typee
+        byte bnum = 100;
+        short snum = 5000;
+        int num = 5;
+        long myNum = 15000000000L;
+
+        float fnum = 6.99f;
+        char cletter = 'D';
+        boolean mybool = true;
+
+        //Non-primitive data type
+        String mytext = "Hello";
+        //Arrays
+        //Classess
+
+        System.out.println(bnum);
+        System.out.println(snum);
+        System.out.println(num);
+        System.out.println(myNum);
+        System.out.println(fnum);
+        System.out.println(cletter);
+        System.out.println(mybool);
+        System.out.println(mytext);
+
+    }
+}
