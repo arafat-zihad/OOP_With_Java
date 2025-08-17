cd6cf5edf16f68c5a03a77bfed597b62a4a69183
arafat-zihad <arafat.zihad01@gmail.com>
2025-08-17 22:24:33 +0600

    string length

---
+++ Basic_Java/StringL.java
@@ -0,0 +1,6 @@
+public class StringL{
+    public static void main(String[] args){
+        String txt = "abcdeghijklmnopqrstuvwxyz";
+        System.out.println("Lentgh of A to Z is: " + txt.length());
+    }
+}
