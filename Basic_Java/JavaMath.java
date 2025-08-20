a8ba48e7cf3670345d2ee7e0cc45ceac78922ed7
arafat-zihad <arafat.zihad01@gmail.com>
2025-08-20 22:53:40 +0600

    math min

--- Basic_Java/JavaMath.java
+++ Basic_Java/JavaMath.java
@@ -1,6 +1,8 @@
 public class JavaMath{
     public static void main(String[] args){
         int r = Math.max(5, 10);
+        int min = Math.min(5, 10);
         System.out.println(r);
+        System.out.println(min);
     }
 }
