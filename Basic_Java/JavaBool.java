ffb807d5552e4d5588273eb3cfa67b4e32521bff
arafat-zihad <arafat.zihad01@gmail.com>
2025-08-21 00:41:55 +0600

    bool output

---
+++ Basic_Java/JavaBool.java
@@ -0,0 +1,9 @@
+public class JavaBool{
+    public static void main(String[] args) {
+        // int a = 10, b = 20;
+        // boolean r = a > b;
+        int age = 25, votingAge = 18;
+        boolean r = age >= votingAge;
+        System.out.println(r);
+    }
+}
