b90e2281aafadd55faa5a312934abf74d05e9ec8
arafat-zihad <arafat.zihad01@gmail.com>
2025-08-20 23:01:38 +0600

    abs needs double not float

--- Basic_Java/JavaMath.java
+++ Basic_Java/JavaMath.java
@@ -3,9 +3,11 @@ public class JavaMath{
         int r = Math.max(5, 10);
         int min = Math.min(5, 10);
         double sqt = Math.sqrt(64);
+        double ab = Math.abs(-4.7);
     
         System.out.println(r);
         System.out.println(min);
         System.out.println(sqt);
+        System.out.println(ab);
     }
 }
