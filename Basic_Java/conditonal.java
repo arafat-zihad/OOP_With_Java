b73c8c2d91775ad1bad8d87442c8467aefba2718
arafat-zihad <arafat.zihad01@gmail.com>
2025-08-24 23:07:16 +0600

    switch

--- Basic_Java/conditonal.java
+++ Basic_Java/conditonal.java
@@ -1,7 +1,7 @@
 public class Conditional{
     public static void main(String[] args) {
         int x = 10, y = 20;
-        if(x < 10){
+        if(x < y){
             System.out.println("Correct");
         }
         else{
