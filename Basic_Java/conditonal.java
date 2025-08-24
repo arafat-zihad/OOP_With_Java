0e6761b7caadafbe260e7c89b88d23b4121bcaad
arafat-zihad <arafat.zihad01@gmail.com>
2025-08-24 23:04:54 +0600

    Correct the file name

---
+++ Basic_Java/conditonal.java
@@ -0,0 +1,11 @@
+public class conditional{
+    public static void main(String[] args) {
+        int x = 10, y = 20;
+        if(x < 10){
+            System.out.println("Correct");
+        }
+        else{
+            System.out.println("Wrong");
+        }
+    }
+}
