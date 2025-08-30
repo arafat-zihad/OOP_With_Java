b99751b95dd3c8b0ab716c811703eab1bcde4adb
arafat-zihad <arafat.zihad01@gmail.com>
2025-08-30 23:03:32 +0600

    loop

---
+++ Basic_Java/Loop.java
@@ -0,0 +1,10 @@
+
+
+public class Loop{
+    public static void main(String[] args) {
+        int n = 10;
+        for(int i = 0; i <= n; i++){
+            System.err.println(i);
+        }
+    }
+}
