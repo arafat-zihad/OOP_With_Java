0a034c96a1b7be1a90addeb68013d18cc7a7906e
arafat-zihad <arafat.zihad01@gmail.com>
2025-09-05 09:43:48 +0600

    Anisul Islam Product assaginment

---
+++ Basic_Java/Product.java
@@ -0,0 +1,12 @@
+//Anisul islam assignment 005 video
+
+public class Product{
+    public static void main(String[] args) {
+        System.out.println("Product Id: 101");
+        System.out.println("Title: iPhone19");
+        System.out.println("Price: 1895 Eur");
+        System.out.println("Description: Perfect product with best pricing");
+        System.out.println("Category: Phone");
+
+    }
+}
