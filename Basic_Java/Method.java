bbf479f777463c115352a7863a45e109c8a1fdf6
arafat-zihad <arafat.zihad01@gmail.com>
2025-09-15 22:06:26 +0600

    Class and Object new file

--- Basic_Java/Method.java
+++ Basic_Java/Method.java
@@ -1,45 +1,42 @@
-class Teacher{
+
+class Teacher {
+
     String name;
     int age;
     String gender;
     int phone;
 
-    void show(){
-    System.out.println("Name: " + name);
-    System.out.println("Age: "+ age);
-    System.out.println("Gender: " + gender);
-    System.out.println("Phone: " + phone);
+    void show() {
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Gender: " + gender);
+        System.out.println("Phone: " + phone);
     }
 }
 
-public class Method{ 
-public static void main(String[] args){
-    System.out.println("");
-    //Without method
-    Teacher teacher1 = new Teacher();
-    teacher1.name = "Anisul Islam";
-    teacher1.age = 28;
-    teacher1.gender = "Male";
-    teacher1.phone = 128382929;
+public class Method {
 
-    System.out.println(teacher1.name);
-    System.out.println(teacher1.age);
-    System.out.println(teacher1.gender);
-    System.out.println(teacher1.phone);
+    public static void main(String[] args) {
+        
+        Teacher teacher1 = new Teacher();
+        teacher1.name = "Anisul Islam";
+        teacher1.age = 28;
+        teacher1.gender = "Male";
+        teacher1.phone = 128382929;
 
-System.out.println("");
+        teacher1.show();
 
-    //Using method
-    Teacher teacher2;
-    teacher2 = new Teacher();
+        System.out.println("");
 
-    teacher2.name = "Ruhul Amin";
-    teacher2.age = 28;
-    teacher2.gender = "Male";
-    teacher2.phone = 10344242;
+        Teacher teacher2;
+        teacher2 = new Teacher();
 
-    teacher2.show();
-}
+        teacher2.name = "Ruhul Amin";
+        teacher2.age = 30;
+        teacher2.gender = "Male";
+        teacher2.phone = 10344242;
 
+        teacher2.show();
+    }
 
 }
---
+++ Class_Object/Main.java
@@ -0,0 +1,45 @@
+class Teacher{
+    String name;
+    int age;
+    String gender;
+    int phone;
+
+    void show(){
+    System.out.println("Name: " + name);
+    System.out.println("Age: "+ age);
+    System.out.println("Gender: " + gender);
+    System.out.println("Phone: " + phone);
+    }
+}
+
+public class Main{ 
+public static void main(String[] args){
+    System.out.println("");
+    //Without method
+    Teacher teacher1 = new Teacher();
+    teacher1.name = "Anisul Islam";
+    teacher1.age = 28;
+    teacher1.gender = "Male";
+    teacher1.phone = 128382929;
+
+    System.out.println(teacher1.name);
+    System.out.println(teacher1.age);
+    System.out.println(teacher1.gender);
+    System.out.println(teacher1.phone);
+
+System.out.println("");
+
+    //Using method
+    Teacher teacher2;
+    teacher2 = new Teacher();
+
+    teacher2.name = "Ruhul Amin";
+    teacher2.age = 28;
+    teacher2.gender = "Male";
+    teacher2.phone = 10344242;
+
+    teacher2.show();
+}
+
+
+}
