d6585eb98e76abf4e16c6b47a89b110f837ed64b
arafat-zihad <arafat.zihad01@gmail.com>
2025-12-08 22:25:26 +0600

    14 done

---
+++ Practice/14/Main.java
@@ -0,0 +1,14 @@
+public class Main{
+    public static void main(String[] args) {
+        Pet Dog = new Pet();
+        Pet Cat = new Pet();
+        Dog.pet_name = "Tom";
+        Dog.age = 2;
+        Dog.display();
+
+        Cat.pet_name = "Garwa";
+        Cat.age = 1;
+        Cat.display();
+
+    }
+}
---
+++ Practice/14/Pet.java
@@ -0,0 +1,9 @@
+public class Pet{
+    String pet_name;
+    int age;
+
+    void display(){
+        System.out.println("Name: " + pet_name);
+        System.out.println("Age: " + age);
+    }
+}
