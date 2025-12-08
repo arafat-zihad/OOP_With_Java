36b10f0baf24b9fad2698b278ba14e382ea862b4
arafat-zihad <arafat.zihad01@gmail.com>
2025-12-08 22:58:33 +0600

    problem 16th solved

---
+++ Practice/16/Main.java
@@ -0,0 +1,6 @@
+public class Main{
+    public static void main(String[] args) {
+        Employee obj1 = new Manager();
+        obj1.work();
+    }
+}
---
+++ Practice/16/Manager.java
@@ -0,0 +1,8 @@
+public class Manager extends Employee{
+    public void work(){
+        System.out.println("Manager managing");
+    }
+    public void manageTeam(){
+        System.out.println("Managing team");
+    }
+}
