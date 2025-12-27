456fd6cf987c98f0fbd31fea0daab9d2c7391cf5
arafat-zihad <arafat.zihad01@gmail.com>
2025-12-27 14:44:01 +0600

    60 b ct 1 solved

---
+++ 60B_CT1_SET_B/Main.java
@@ -0,0 +1,29 @@
+class Car{
+    String brand;
+    Car(String b){
+        this.brand = b;
+    }
+    void startEngine(){
+        System.out.println("Engine started for car: " + brand);
+    }
+}
+
+class Truck extends Car{
+    int loadCapacity;
+    Truck(String b, int capacity){
+        super(b);
+        loadCapacity = capacity;
+    }
+    void loadTruck(){
+        System.out.println("Truck loaded with capacity: " + loadCapacity + "Kg");
+    }
+}
+
+public class Main{
+    public static void main(String[] args) {
+        Car c = new Car("Tesla");
+        Truck t = new Truck("Velvo", 5000);
+        t.loadTruck();
+        c.startEngine();
+    }
+}
